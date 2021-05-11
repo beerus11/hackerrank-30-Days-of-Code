486fd0729391b3aa3122e234bd622d5819235d2a
Dakshitha Dissanayaka <45692186+Dakshitha96@users.noreply.github.com>
2021-05-11 22:11:14 +0530

    Update Day-4-Class-vs-Instance.java

--- Day-4-Class-vs-Instance.java
+++ Day-4-Class-vs-Instance.java
@@ -1,27 +1,36 @@
-public class Person {
-    private int age;
+import java.io.*;
+import java.util.*;
 
-	public Person(int initialAge) {
-  		// Add some more code to run some checks on initialAge
-        this.age=initialAge;
-	}
+public class Person {
+    private int age;    
+  
+    public Person(int initialAge) {
+        // Add some more code to run some checks on initialAge
+          
+          if (initialAge > 0) {
+                age = initialAge;
+            } else {
+                System.out.println("Age is not valid, setting age to 0.");
+                age = 0;
+            }
+    }
 
-	public void amIOld() {
-  		// Write code determining if this person's age is old and print the correct statement:
-        if(this.age<0)
-          { System.out.println("Age is not valid, setting age to 0.");
-           this.age=0;
-           amIOld();
-          }
-        else if(this.age<13)
-            System.out.println("You are young.");
-        else if(this.age<18)
-            System.out.println("You are a teenager.");
-        else if(this.age>=18)
-            System.out.println("You are old.");
-	}
+    public void amIOld() {
+        // Write code determining if this person's age is old and print the correct statement:
+        String output="";
+        if (age < 13) {
+            output="You are young.";
+        }else if (age < 18) {
+            output="You are a teenager.";
+        }else {
+            output="You are old.";
+        }
+        System.out.println(output);
+    }
 
-	public void yearPasses() {
-  		// Increment this person's age.
-        this.age+=1;
-	}
+    public void yearPasses() {
+        // Increment this person's age.
+          age++;
+    }
+
+    public static void main(String[] args) {
